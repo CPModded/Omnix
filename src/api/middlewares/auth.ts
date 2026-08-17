3fbf59936fb3e22a1c8f732ea37391a5513b6de4
CPModded <massgrc5@gmail.com>
2026-08-17 04:34:05 +0200

    Add files via upload

--- src/api/middlewares/auth.ts
+++ src/api/middlewares/auth.ts
@@ -6,7 +6,7 @@
 
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import { CONFIG } from '../../config.ts';
+import { CONFIG } from '../../config/index.ts';
 
 export interface AuthenticatedRequest extends Request {
   user?: {
