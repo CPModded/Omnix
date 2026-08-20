130d5579a7a3defc44f53c72b8d653e9f3ef2e17
CPModded <massgrc5@gmail.com>
2026-08-20 15:23:42 +0200

    Delete src/api directory

--- src/api/middlewares/src 2/api/routes/guilds.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { GuildsController } from '../controllers/guilds.controller';
-import { isAuthenticated } from '../middlewares/auth';
-
-const router = Router();
-router.get('/', isAuthenticated as any, GuildsController.getUserGuilds);
-
-export default router;
--- src/api/middlewares/src 2/api/routes/license.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { LicenseController } from '../controllers/license.controller';
-import { isAuthenticated } from '../middlewares/auth';
-
-const router = Router();
-
-router.post('/activate', isAuthenticated as any, LicenseController.activateLicense);
-
-export default router;
