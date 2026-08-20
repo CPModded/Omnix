0ff90d00a8782952e37753906d0726d716af0032
CPModded <massgrc5@gmail.com>
2026-08-20 20:47:29 +0200

    Add files via upload

--- src/api/controllers/guilds.controller.ts
+++ src/api/controllers/guilds.controller.ts
@@ -8,7 +8,7 @@ import type { Response } from 'express';
 import axios from 'axios';
 import { ChannelType } from 'discord.js';
 import type { AuthenticatedRequest } from '../middlewares/auth.ts';
-import { adminAccessCache } from '../middlewares/guildAuth.ts';
+import type { adminAccessCache } from '../middlewares/guildAuth.ts';
 import { client as botClient } from '../../bot/client.ts';
 
 const userGuildsCache = new Map<string, { guilds: any[]; expiresAt: number }>();
